d812f88f921719e0d9d1b38e1e474bdc79c9789d
mountassir-007 <ahmedelmountassir10@gmail.com>
2022-02-15 18:15:32 +0100

    time function

---
+++ philo_m/time_handlers.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   time_handlers.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahel-mou <ahel-mou@1337.ma>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/31 15:50:27 by ahel-mou          #+#    #+#             */
+/*   Updated: 2022/02/14 21:01:14 by ahel-mou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+unsigned long	get_time(void)
+{
+	struct timeval		tv;
+	unsigned long		time_ms;
+
+	gettimeofday(&tv, NULL);
+	time_ms = tv.tv_sec * 1000 + tv.tv_usec / 1000;
+	return (time_ms);
+}
+
+void	wait_for(int time_to_wait)
+{
+	unsigned long	time;
+
+	time = get_time();
+	while (get_time() < time + (unsigned long)time_to_wait)
+		usleep(500);
+}
